5b7a2036d4548112e4e485b578ab8a671296a525
hecside <hzamoran@asu.edu>
2020-07-02 02:55:17 -0700

    Updates

---
+++ precache-manifest.077139b3d8ca6895f69c72861ed71a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab42d6ed346dda3dfe2ede61c452107",
+    "url": "/monster-roladex/index.html"
+  },
+  {
+    "revision": "7dddb2b36bb3d95018e9",
+    "url": "/monster-roladex/static/css/main.1bf83626.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-roladex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roladex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dddb2b36bb3d95018e9",
+    "url": "/monster-roladex/static/js/main.ebc3519b.chunk.js"
+  },
+  {
+    "revision": "7c50e749f0a8f80afd24",
+    "url": "/monster-roladex/static/js/runtime-main.f8ee6ecd.js"
+  }
+]);
