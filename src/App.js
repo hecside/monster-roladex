ec2c954ece08dcc54a636dea3679e74463d116c1
hecside <hzamoran@asu.edu>
2020-07-03 00:06:14 -0700

    changed title name

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ handleChange = (e)=>{
       )
     return (
       <div className="App">
-      <h1>Monsters Roladex</h1>
+      <h1>Monsters Rolodex</h1>
       <SearchBox 
       placeholder='search monsters'
       handleChange={this.handleChange}
